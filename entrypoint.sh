a773c9855921466ebe44e90bd92aefb4ddcbeb2e
Dante Barba <dantebarba@gmail.com>
2019-05-11 19:03:55 -0300

    formatting

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,65 +4,57 @@ set -e
 
 # Re-write cron shortcut
 case "$(echo "$CRON" | tr '[:lower:]' '[:upper:]')" in
-    *@YEARLY* ) echo "INFO: Cron shortcut $CRON re-written to 0 0 1 1 *" && CRONS="0 0 1 1 *";;
-    *@ANNUALLY* ) echo "INFO: Cron shortcut $CRON re-written to 0 0 1 1 *" && CRONS="0 0 1 1 *";;
-    *@MONTHLY* ) echo "INFO: Cron shortcut $CRON re-written to 0 0 1 * *" && CRONS="0 0 1 * * ";;
-    *@WEEKLY* ) echo "INFO: Cron shortcut $CRON re-written to 0 0 * * 0" && CRONS="0 0 * * 0";;
-    *@DAILY* ) echo "INFO: Cron shortcut $CRON re-written to 0 0 * * *" && CRONS="0 0 * * *";;
-    *@MIDNIGHT* ) echo "INFO: Cron shortcut $CRON re-written to 0 0 * * *" && CRONS="0 0 * * *";;
-    *@HOURLY* ) echo "INFO: Cron shortcut $CRON re-written to 0 * * * *" && CRONS="0 * * * *";;
-    *@* ) echo "WARNING: Cron shortcut $CRON is not supported. Stopping." && exit 1;;
-    * ) CRONS=$CRON;;
+*@YEARLY*) echo "INFO: Cron shortcut $CRON re-written to 0 0 1 1 *" && CRONS="0 0 1 1 *" ;;
+*@ANNUALLY*) echo "INFO: Cron shortcut $CRON re-written to 0 0 1 1 *" && CRONS="0 0 1 1 *" ;;
+*@MONTHLY*) echo "INFO: Cron shortcut $CRON re-written to 0 0 1 * *" && CRONS="0 0 1 * * " ;;
+*@WEEKLY*) echo "INFO: Cron shortcut $CRON re-written to 0 0 * * 0" && CRONS="0 0 * * 0" ;;
+*@DAILY*) echo "INFO: Cron shortcut $CRON re-written to 0 0 * * *" && CRONS="0 0 * * *" ;;
+*@MIDNIGHT*) echo "INFO: Cron shortcut $CRON re-written to 0 0 * * *" && CRONS="0 0 * * *" ;;
+*@HOURLY*) echo "INFO: Cron shortcut $CRON re-written to 0 * * * *" && CRONS="0 * * * *" ;;
+*@*) echo "WARNING: Cron shortcut $CRON is not supported. Stopping." && exit 1 ;;
+*) CRONS=$CRON ;;
 esac
 
 # Set time zone if passed in
-if [ ! -z "$TZ" ]
-then
+if [ ! -z "$TZ" ]; then
   cp /usr/share/zoneinfo/$TZ /etc/localtime
-  echo $TZ > /etc/timezone
+  echo $TZ >/etc/timezone
 fi
 
 rm -f /tmp/sync.pid
 
-  # SYNC_SRC and SYNC_DEST setup
-  # run sync either once or in cron depending on CRON
+# SYNC_SRC and SYNC_DEST setup
+# run sync either once or in cron depending on CRON
 
-  #Create fail URL if CHECK_URL is populated but FAIL_URL is not 
-  if [ ! -z "$CHECK_URL" ] && [ -z "$FAIL_URL" ]
-  then
-    FAIL_URL="${CHECK_URL}/fail"
-  fi
+#Create fail URL if CHECK_URL is populated but FAIL_URL is not
+if [ ! -z "$CHECK_URL" ] && [ -z "$FAIL_URL" ]; then
+  FAIL_URL="${CHECK_URL}/fail"
+fi
 
-  if [ -z "$CRONS" ]
-  then
-    echo "INFO: No CRON setting found. Running sync once."
-    echo "INFO: Add CRON=\"0 0 * * *\" to perform sync every midnight"
-    /sync.sh
+if [ -z "$CRONS" ]; then
+  echo "INFO: No CRON setting found. Running sync once."
+  echo "INFO: Add CRON=\"0 0 * * *\" to perform sync every midnight"
+  /sync.sh
+else
+  if [ -z "$FORCE_TEST" ]; then
+    echo "INFO: Add FORCE_TEST=1 to perform a test upon boot"
   else
-    if [ -z "$FORCE_TEST" ]
-    then
-      echo "INFO: Add FORCE_TEST=1 to perform a test upon boot"
-    else
-      /sync.sh
-    fi
-
-    # Setup logs
-    d=$(date +%Y_%m_%d)
-    LOG_FILE="/tmp/$d.log"
-    echo "INFO: Log file output to $LOG_FILE"
-
-    # Setup cron schedule
-    crontab -d
-    echo "$CRONS /sync.sh >> $LOG_FILE 2>&1" > /tmp/crontab.tmp
-    echo "TEST_URL is: $TEST_URL"
-    crontab /tmp/crontab.tmp
-    rm /tmp/crontab.tmp
-
-    # Start cron
-    echo "INFO: Starting crond ..."
-    touch $LOG_FILE
-    touch /tmp/crond.log
-    crond -b -l $LOG_LEVEL -L /tmp/crond.log
-    echo "INFO: crond started"
-    tail -F /tmp/crond.log $LOG_FILE
+    /sync.sh
   fi
+
+  # Setup logs
+  LOG_FILE="/tmp/$(date +%Y_%m_%d).log"
+  echo "INFO: Log file output to $LOG_FILE"
+
+  # Setup cron schedule
+  crontab -d
+  echo "$CRONS /sync.sh >> $($LOG_FILE) 2>&1" >/tmp/crontab.tmp
+  echo "TEST_URL is: $TEST_URL"
+  crontab /tmp/crontab.tmp
+  rm /tmp/crontab.tmp
+
+  # Start cron
+  echo "INFO: Starting crond ..."
+  touch /tmp/crond.log
+  crond -f -l $LOG_LEVEL -L /tmp/crond.log
+fi
--- sync.sh
+++ sync.sh
@@ -5,32 +5,27 @@ set -e
 echo "INFO: Starting sync.sh pid $$ $(date)"
 
 # Delete logs by user request
-if [ ! -z "${ROTATE_LOG##*[!0-9]*}" ]
-then
+if [ ! -z "${ROTATE_LOG##*[!0-9]*}" ]; then
   echo "INFO: Removing logs older than $ROTATE_LOG day(s)..."
   find /tmp/*.log -mtime +$ROTATE_LOG -type f -delete
 fi
 
+echo $$ >/tmp/sync.pid
 
-echo $$ > /tmp/sync.pid
-
-export TEST_CODE=200
-    if [ ! -z "$TEST_URL" ] 
-    then
-      TEST_CODE=-1
-      TEST_CODE=$(curl -o /dev/null -s -w "%{http_code}\n" $TEST_URL)
-      echo "INFO: Test output: $TEST_CODE"
-    fi
-    if [ -z "$CHECK_URL" ]
-    then
-      echo "INFO: Define CHECK_URL with https://healthchecks.io to monitor health"
-    else
-      if [ "$TEST_CODE" == 200 ]
-      then
-        wget $CHECK_URL -O /dev/null
-      else
-        wget $FAIL_URL -O /dev/null
-      fi
-    fi
+TEST_CODE=200
+if [ ! -z "$TEST_URL" ]; then
+  TEST_CODE=-1
+  TEST_CODE=$(curl -o /dev/null -s -w "%{http_code}\n" $TEST_URL)
+  echo "INFO: Test output: $TEST_CODE"
+fi
+if [ -z "$CHECK_URL" ]; then
+  echo "INFO: Define CHECK_URL with https://healthchecks.io to monitor health"
+else
+  if [ "$TEST_CODE" == 200 ]; then
+    wget $CHECK_URL -O /dev/null
+  else
+    wget $FAIL_URL -O /dev/null
+  fi
+fi
 
 rm -f /tmp/sync.pid
